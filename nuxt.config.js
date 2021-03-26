ec9c4da6412536ea8b1e119a122cff6e2056f0a2
Owen Liu <liuyu9045+iobit@gmail.com>
2021-03-26 17:39:12 +0800

    test api change

--- nuxt.config.js
+++ nuxt.config.js
@@ -111,7 +111,7 @@ export default {
       ? 'https://www.easygetinsta.com/api'
       : 'https://test.easygetinsta.com/test/api',
     prefix: process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'productionTest'
-      ? '/api'
+      ? '/prod/api'
       : '/dev/test/api',
     credentials: true // 表示跨域请求时是否需要使用凭证
   },
@@ -125,13 +125,13 @@ export default {
         changeOrigin: true
       }
     },
-    // '/prod': {
-    //   target: 'https://www.easygetinsta.com',
-    //   pathRewrite: {
-    //     '^/prod': '',
-    //     changeOrigin: true
-    //   }
-    // }
+    '/prod': {
+      target: 'https://www.easygetinsta.com',
+      pathRewrite: {
+        '^/prod': '',
+        changeOrigin: true
+      }
+    }
   },
 
   serverMiddleware: [
