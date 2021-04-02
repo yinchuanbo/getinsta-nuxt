42ef6581fe2c35b69cbc570cb383059c2925e645
Owen Liu <liuyu9045+iobit@gmail.com>
2021-04-02 15:32:59 +0800

    产品名初始值

--- src/store/index.js
+++ src/store/index.js
@@ -10,8 +10,8 @@ export const state = () => ({
   isAndroid: false,
   isWindows: false,
   cartLength: '',
-  productName: '',
-  productLogo: '',
+  productName: 'GetInsta',
+  productLogo: null,
   hideIosMinorLang: false, // 多语言隐藏与显示
   showIosDe: false, // 多语言隐藏与显示
   minorLangIosLink: '',
