1c5c6552d2a3feb7f50ca52d83ab6a7fff014581
letters123 <yinhaocomeon@gmail.com>
2021-04-08 15:37:51 +0800

    counter 工具页面制作

---
+++ src/views/tools/instagram-follower-counter/instagram-follower-counter.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="instagram-follower-counter">
+        <div class="header-blank pc"></div>
+        <div class="counter-banner">
+            <h2>Live Instagram Follower Counter</h2>
+            <p>Real-time, accurate, exact Instagram follower count. No Instagram account log in.</p>
+            <div class="counter-banner_inputs">
+                <label>
+                    <input
+                    v-model="searchInsInput"
+                    :disabled="searchInsLoading"
+                    type="text"
+                    placeholder="Type Instagram Username"
+                    :style="{
+                        borderColor: isBoxRed ? 'red!important' : '#E0E1E6!important',
+                    }"
+                    @input="changeInput"
+                    @blur="inputBlur"
+                    @focus="inputFocus"
+                    />
+                </label>
+                <button>Check Now</button>
+            </div>
+            <div class="counter-download"><a href="">Download</a> GetInsta to increase your Instagram follower count</div>
+        </div>
+        <div class="counter-container">
+            <h2>Introducing You to the Instagram Follower Counter</h2>
+            <p>Learn how Instagram follower counter could help you upgrage your Instagram management and efficiency by checking content below.</p>
+            <div class="counter-container-item1"></div>
+            <div class="counter-container-item2"></div>
+            <div class="counter-container-item3"></div>
+            <div class="counter-container-item4"></div>
+            <div class="counter-container-item5"></div>
+            <div class="counter-container-item6"></div>
+        </div>
+        <div class="counter-signup"></div>
+        <div class="counter-why">
+            <div class="counter-why-item"></div>
+            <div class="counter-why-item"></div>
+            <div class="counter-why-item"></div>
+            <div class="counter-why-item"></div>
+        </div>
+        <div class="counter-tips">
+            <div class="counter-tips-item"></div>
+            <div class="counter-tips-item"></div>
+            <div class="counter-tips-item"></div>
+            <div class="counter-tips-item"></div>
+            <div class="counter-tips-item"></div>
+            <div class="counter-tips-item"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  components: {
+    
+  }
+}
+</script>
+
+<style scoped lang="scss" src="./instagram-follower-counter.scss"></style>
