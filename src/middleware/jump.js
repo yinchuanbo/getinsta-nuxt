a778fa41c75b24afb4b37685f2ce6a9f62ee3da0
letters123 <yinhaocomeon@gmail.com>
2021-04-21 16:29:01 +0800

    修改 jump

--- src/middleware/jump.js
+++ src/middleware/jump.js
@@ -17,7 +17,7 @@ export default async function ({
         const idArray = paramID.split('-');
         const articleID = idArray.pop();
         if (typeof articleID !== 'string') return;
-        const browserLang = req.headers['accept-language'].split(',')[0];
+        let browserLang = req.headers['accept-language'].toLowerCase().substr(0, 2);
         let locale = '';
         const supportedLocale = ['en', 'fr', 'de', 'es', 'ar', 'it', 'pt'];
         for (let i = 0; i < supportedLocale.length; i++) {
