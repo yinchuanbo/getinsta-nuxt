0f4b692e295d4fcc6506283dcbc7b64e803e6562
letters123 <yinhaocomeon@gmail.com>
2021-04-23 18:20:34 +0800

    修改 _id

--- src/pages/blog/_id.vue
+++ src/pages/blog/_id.vue
@@ -23,21 +23,23 @@ export default {
 
     // ***********************************************************************
     const paramID = route.params.id;
+    if(!req) return;
+
+    console.log('4444');
     if (!paramID) { // 列表页
       DATA.isInstanceDetail = false;
     } else { // 详情页
       const idArray = paramID.split('-');
       const articleID = idArray.pop();
       if (typeof articleID !== 'string') return;
-      // if (isDev) console.log('articleID', articleID);
-      const browserLang = process.client ? navigator.language.toLowerCase().substr(0, 2) : '';
       let locale = 'en';
-      const supportedLocale =  ['en', 'fr', 'de', 'es', 'ar', 'it', 'pt'];
+      let browserLang = req.headers['accept-language'].toLowerCase().substr(0, 2);
+      const supportedLocale = ['en', 'fr', 'de', 'es', 'ar', 'it', 'pt'];
       for (let i = 0; i < supportedLocale.length; i++) {
-        if (supportedLocale[i] === browserLang) {
-          locale = supportedLocale[i];
-          break;
-        }
+          if (supportedLocale[i] === browserLang) {
+              locale = supportedLocale[i];
+              break;
+          }
       }
       let apiParams = {
         article_id: articleID,
@@ -51,9 +53,6 @@ export default {
           blogApi.getBlogDetailV2,
           { params: apiParams }
         );
-
-        console.log(res)
-
         if (res['status'] === 'ok') {
           DATA.reqObj = res;
           app.title = DATA.reqObj['seo_title'];
